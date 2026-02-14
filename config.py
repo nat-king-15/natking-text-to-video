e929547a6b64ab04e59f159a8c6b7cd97e1a9dc8
nat-king-15 <natrajlilhare151@gmail.com>
2026-02-14 11:17:24 +0530

    Fix merge markers in config and requirements

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-<<<<<<< HEAD
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M")
     DB_NAME = os.environ.get("DB_NAME", "nattu")
     API_ID = int(os.environ.get("API_ID", 6886135))
@@ -25,15 +24,3 @@ class Config(object):
 
     USERLINK = os.environ.get("USERLINK", "")
     TUTORIAL_VIDEO = os.environ.get("TUTORIAL_VIDEO", "")
-=======
-    BOT_TOKEN = "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M"
-    DB_NAME = "nattu"
-    API_ID = "6886135"
-    API_HASH = "ee20a1c8a8e44eaa638b7254cbcc3012"
-    ADMIN_ID = [2118600611]
-    DB_URL = "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0"
-    LOG_CHANNEL = -1003744110162 # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = ""
-    TUTORIAL_VIDEO = ""
->>>>>>> 595a6caeb08f901ec90a4bfed6e34f32cb15f88e
-
